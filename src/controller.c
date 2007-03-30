60cf4e7ae5837052a7985a0f6fcc5d36add7dc86
mkarir <mkarir@ed687b79-fb24-0410-8171-41e69a8c0a6b>
2007-03-30 20:00:11 +0000

    incorporating latest patch from Jesse Norell <jesse@kci.net>  - lots of cleanups and error checking and validation  - I dropped from the submitted patch the modifications to the    way the connect command is sent between the controller and collector    as it did'nt seem to work right after merging  - also dropped from the patch the automatic switch to user USER    and group GROUP if the collector is run as root perhaps we will    add this back in later when we have better defined install process    that walks people thru the creation of a separate calea user    for now it would just get in the way of people being able to use    it easily(testing).   - Important: lea_collection.c and lea_collection.h have been     renamed to lea_collector.c and lea_collector.h  this is really what    the original was meant to be the original typo lasted too long.    Sorry for the filename change mid-stream.   - this includes the __FAVOR_BSD patch for debian systems to work right.

--- src/controller.c
+++ src/controller.c
@@ -30,12 +30,11 @@
 
 #include <fcntl.h>
 
-#define MAX_CONNECTIONS 10
 #define MAX_MSGSIZE 1024 
 
 #define PROMPT "CALEA-CTRL> "
 
-int controller_socket = 0;
+int controller_socket = -1;
 struct sockaddr_in collector_addr;
 
 int connected_flag = 0;
@@ -68,55 +67,97 @@ int process_response ( char* recv_buf ) {
 int process_user_cmd ( char* user_input, char* msgbuf ) {
 
     char cmd[64];
-    int retval;
+    memset ( cmd, '\0', 64 );
  
+    if ( strlen ( user_input ) >= MAX_MSGSIZE ) {
+        fprintf ( stderr, "Input too long.\n" );
+        exit ( -1 );
+    }
     if ( strcmp ( user_input, "\n" ) == 0 ) {
         return 0;
     }
-    sscanf ( user_input, "%s", cmd );
+    sscanf ( user_input, "%63s", cmd );
 
     if ( strcmp ( cmd, "batch-start" ) == 0 ) {
-        if ( controller_socket != 0 ) {
+        if ( controller_socket > 0 ) {
             close ( controller_socket );
+            controller_socket = -1;
         }
         batch_mode = 1;
         return 1;
     } 
     if ( strcmp ( cmd, "batch-stop" ) == 0 ) {
-        if ( controller_socket != 0 ) {
+        if ( controller_socket > 0 ) {
             close ( controller_socket );
+            controller_socket = -1;
+            connected_flag = 0;
         }
         batch_mode = 2;
         return 1;
     } 
 
+    if ( ( strcmp ( cmd, "exit" ) == 0 ) || ( strcmp ( cmd, "quit" ) == 0 ) ) {
+        if ( connected_flag ) {
+            snprintf ( msgbuf, 64, "%d", CLOSE_SESSION );
+            return 1;
+        } else {
+            printf ("Closing.\n");
+            exit ( 0 );
+        }
+        return 0;
+    } 
+
+    if ( strcmp ( cmd, "help" ) == 0 ) {
+        print_help_msg ( );
+        return 0;
+    } 
+
     if ( (strcmp ( cmd, "connect" ) != 0) && ( !connected_flag ) ) {
-        printf ("you must be connected first before issuing this command...\n");
+        printf ("Error: Bad Command. (maybe \"connect\" first?)\n\n");
+        print_help_msg ( );
         return 0;
     }
 
     if ( strcmp ( cmd, "connect" ) == 0 ) {
         char *collector_ipaddr;
-        collector_ipaddr = user_input + strlen ( cmd ) + 1;
+
+        /* how large do we need to support ipv6 addrs? */
+        if (! ( collector_ipaddr = (char*) malloc ( 16 ) ) ) {
+           perror("malloc");
+            exit ( -1 );
+        }
+        memset ( collector_ipaddr, '\0', 16 );
+
+        sscanf ( user_input, "%63s %15s", cmd, collector_ipaddr );
 
         controller_socket = socket ( AF_INET, SOCK_STREAM, 0 );
         if ( controller_socket == -1 ) {
-            printf ( "Error while creating server socket\n" );
+            perror ( "Error while creating server socket: socket" );
             exit ( -1 );
         }
-        fcntl ( controller_socket, F_SETFL, O_NONBLOCK );        
+        if (fcntl ( controller_socket, F_SETFL, O_NONBLOCK ) == -1) {
+            perror ( "fcntl" );
+            exit ( -1 );
+        }
 
         collector_addr.sin_family = AF_INET;    
-        collector_addr.sin_port = htons ( 5555 ) ;
+        collector_addr.sin_port = htons ( Collector_PORT ) ;
         collector_addr.sin_addr.s_addr = inet_addr( collector_ipaddr );
+        free ( collector_ipaddr );
+
+        if (collector_addr.sin_addr.s_addr == INADDR_NONE) {
+            printf ( "Bad IP Addr\n" );
+            return 0;
+        }
  
         int i = 0;
         printf ( "Connecting." );
         fflush ( stdout );
         for ( i = 0; i < 5 ; i++ ) {
-            connect ( controller_socket, 
+            if (connect ( controller_socket, 
                            (struct sockaddr*) &collector_addr, 
-                           sizeof(struct sockaddr_in) );
+                           sizeof(struct sockaddr_in) )  == 0 )
+                break;
             sleep ( 1 );
             if ( errno == EISCONN ) {
                 break;
@@ -125,48 +166,67 @@ int process_user_cmd ( char* user_input, char* msgbuf ) {
             fflush ( stdout );
         }
         printf ( "\n" );
-        retval = fcntl ( controller_socket, F_SETFL, 2 );        
-       
-        /* if socket is already connected */
+        fcntl ( controller_socket, F_SETFL, 2 );
+
+/* what is "2"?  maybe FWRITE?  need to check retval if applicable.
+   or is this just to check socket connection status? */
+
         if ( errno == EISCONN ) {
             if ( batch_mode == 0 ) {
                 connected_flag = 1;
             }
-            snprintf ( msgbuf, 64, "%d", CONNECT ); 
+            snprintf ( msgbuf, 64, "%d", CONNECT );
         } else {
-            printf ( "connect failed...\n" );
+            printf ( "Connect failed.\n" );
         }
+
+
+//        snprintf ( msgbuf, 64, "%d", NOP );
+//        if ( send ( controller_socket, msgbuf, strlen ( msgbuf ), 0 ) == -1 ) {
+//            memset ( msgbuf, '\0', strlen ( msgbuf ) );
+//            /* if socket is already connected */
+//            if ( errno == EPIPE ) {
+//                if ( batch_mode == 0 )
+//                    connected_flag = 1;
+//                snprintf ( msgbuf, 64, "%d", CONNECT );
+//            } else {
+//                perror ( "Connect failed" );
+//                return 0;
+//           }
+//        } else {
+//            if ( batch_mode == 0 )
+//                connected_flag = 1;
+//            memset ( msgbuf, '\0', strlen ( msgbuf ) );
+//            snprintf ( msgbuf, 64, "%d", CONNECT );
+//        }
+//
         return 1;
-    } 
+    }
     if ( strcmp ( cmd, "start" ) == 0 ) {
         char *filter;
         int batch_id = 0;
 
-        filter = user_input + strlen ( cmd ) + 1;
-        snprintf ( msgbuf, 2048, "%d %d %s", TAP_START, batch_id, filter ); 
+        if (! ( filter = (char*) malloc ( MAX_MSGSIZE ) ) ) {
+           perror("malloc");
+            exit ( -1 );
+        }
+        memset ( filter, '\0', MAX_MSGSIZE );
+
+        sscanf ( user_input, " %63s ", cmd );
+        strncpy ( filter, &user_input[ strlen ( cmd ) ], MAX_MSGSIZE - strlen ( cmd ) );
+        snprintf ( msgbuf, MAX_MSGSIZE, "%d %d %s", TAP_START, batch_id, filter ); 
+        free ( filter );
         return 1;
     } 
     if ( strcmp ( cmd, "stop" ) == 0 ) {
         int target_pid = 0;
-        sscanf ( user_input, "%s %d", cmd, &target_pid );
+        sscanf ( user_input, "%63s %d", cmd, &target_pid );
         snprintf ( msgbuf, 64, "%d %d %d", TAP_STOP, 0, target_pid ); 
         return 1;
     } 
     if ( strcmp ( cmd, "status" ) == 0 ) {
-        snprintf ( msgbuf, 64, "%d", SHOW_PROCESS_REGISTRY ); 
+        snprintf ( msgbuf, 64, "%d", SHOW_PROCESS_REGISTRY );
         return 1;
-    } 
-    if ( strcmp ( cmd, "quit" ) == 0 ) {
-        snprintf ( msgbuf, 64, "%d", CLOSE_SESSION ); 
-        return 1;
-    } 
-    if ( strcmp ( cmd, "exit" ) == 0 ) {
-        snprintf ( msgbuf, 64, "%d", CLOSE_SESSION ); 
-        return 1;
-    } 
-    if ( strcmp ( cmd, "help" ) == 0 ) {
-        print_help_msg ( );
-        return 0;
     } 
     
     return -1;
@@ -181,9 +241,18 @@ int main ( void ) {
     printf ( "Starting client...\n" );
     srand (getpid());
 
-    msgbuf = (char*) malloc ( 8 * MAX_MSGSIZE );
-    user_input = (char*) malloc ( 8 * MAX_MSGSIZE );
-    return_buf = (char*) malloc ( 2048 * 8 );
+    if (! ( msgbuf = (char*) malloc ( MAX_MSGSIZE ) ) ) {
+       perror("malloc");
+        exit ( -1 );
+    }
+    if (! ( user_input = (char*) malloc ( MAX_MSGSIZE ) ) ) {
+       perror("malloc");
+        exit ( -1 );
+    }
+    if (! ( return_buf = (char*) malloc ( MAX_MSGSIZE ) ) ) {
+       perror("malloc");
+        exit ( -1 );
+    }
     int send_len = 0;
     int recv_len = 0;
     int retval = 0;
@@ -191,28 +260,41 @@ int main ( void ) {
     /* process user commands till we exit */
     while ( 1 ) {
         printf ( "%s ", PROMPT );
-        memset ( user_input, '\0', 8 * MAX_MSGSIZE );
-        memset ( msgbuf, '\0', 8 * MAX_MSGSIZE );
-        fgets ( user_input, 100, stdin );
+        memset ( user_input, '\0', MAX_MSGSIZE );
+        memset ( msgbuf, '\0', MAX_MSGSIZE );
+
+        if ( fgets ( user_input, MAX_MSGSIZE, stdin ) == NULL ) {
+            perror( "fgets" );
+            exit( -1 );
+        }
 
         retval = process_user_cmd ( user_input, msgbuf );
 
         if ( batch_mode == 0 ) {
             if ( retval == 1) {
-                send_len = send ( controller_socket, msgbuf, MAX_MSGSIZE, 0 );
-                memset ( return_buf, '\0', 2048 );
-                recv_len = recv ( controller_socket, return_buf, 2048, 0 ); 
+                if ( (send_len = send ( controller_socket, msgbuf, strlen ( msgbuf ), 0 )) == -1 ) {
+                    perror("send");
+                    exit (-1);
+                }
+                memset ( return_buf, '\0', MAX_MSGSIZE );
+                if ( (recv_len = recv ( controller_socket, return_buf, MAX_MSGSIZE, 0 )) == -1 )
+                    if ( errno != EAGAIN ) {
+                        perror("recv");
+                        exit (-1);
+                    }
                 if ( recv_len  >  0 ) {
                     int resp_code;
                     resp_code = process_response ( return_buf );
-                    if ( resp_code == atoi(QUIT) ) {
-                        /* collector ACKed our request to quit */
-                        break;
-                    } else if ( resp_code == atoi(ACK) ) {
-                        /* collector ACKed our request */
-    
-                    } else {
-                        printf ( "%s\n", return_buf );
+                    switch ( resp_code ) {
+                        case QUIT:
+                            /* collector ACKed our request to quit */
+                            break;
+                        case ACK:
+                            /* collector ACKed our request */
+                            break;
+                        default:
+                            printf ( "%s\n", return_buf );
+                            break;
                     }
                 }
             } else {
@@ -233,10 +315,10 @@ int main ( void ) {
             char *tok;
             int end;
             char cmd[64];
-            char my_input[2048];
+            char my_input[MAX_MSGSIZE];
             int batch_id = rand();
 
-            sscanf ( user_input, "%s", cmd );
+            sscanf ( user_input, "%63s", cmd );
             batch_cmd = user_input + strlen ( cmd ) + 1;
 
             if ( batch_mode == 1 ) {
@@ -268,25 +350,32 @@ int main ( void ) {
             while (start < str_end) {
                 tok = strtok ( start, " " );
                 printf ("tok: %s\n", tok);
-                snprintf ( my_input, 2048, "connect %s", tok );
+                snprintf ( my_input, MAX_MSGSIZE, "connect %s", tok );
                 retval = process_user_cmd ( my_input, msgbuf );
                 if ( batch_mode == 1 ) {
-                    snprintf ( msgbuf, 2048, "%d %d %s", 
+                    snprintf ( msgbuf, MAX_MSGSIZE, "%d %d %s", 
                                TAP_START, batch_id, filter ); 
                 } else if ( batch_mode == 2 ) {
-                    snprintf ( msgbuf, 2048, "%d %d %d", TAP_STOP, batch_id, 0 ); 
+                    snprintf ( msgbuf, MAX_MSGSIZE, "%d %d %d", TAP_STOP, batch_id, 0 ); 
                 }
-                send_len = send ( controller_socket, msgbuf, MAX_MSGSIZE, 0 );
-                snprintf ( msgbuf, 64, "%d", CLOSE_SESSION ); 
-                send_len = send ( controller_socket, msgbuf, MAX_MSGSIZE, 0 );
+                if ( (send_len = send ( controller_socket, msgbuf, strlen ( msgbuf ), 0 )) == -1 ) {
+                    perror("send");
+                    exit (-1);
+                }
+                snprintf ( msgbuf, 64, "%d", CLOSE_SESSION );
+                if ( (send_len = send ( controller_socket, msgbuf, strlen ( msgbuf ), 0 )) == -1 ) {
+                    perror("send");
+                    exit (-1);
+                }
 
                 start = start + strlen ( tok ) + 1;
-                memset ( msgbuf, '\0', 8 * MAX_MSGSIZE);
+                memset ( msgbuf, '\0', MAX_MSGSIZE);
             }
             batch_mode = 0;
             connected_flag = 0;
-        } 
-        memset ( msgbuf, '\0', 8 * MAX_MSGSIZE);
+            if ( filter )
+                free ( filter );
+        }
     }
 
     free ( return_buf );
@@ -294,6 +383,7 @@ int main ( void ) {
     free ( msgbuf );
 
     close( controller_socket );
+    controller_socket = -1;
 
     return 0;
 }
